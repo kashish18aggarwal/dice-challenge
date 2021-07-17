97348be1fd31e3f55219ecc2fb9f196987d9eed2
kashish18aggarwal <85881672+kashish18aggarwal@users.noreply.github.com>
2021-07-17 19:07:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+alert("Are u ready to play?");
+var p1 = prompt("Enter Your name");
+	
 var r1 = Math.floor(Math.random()*6) + 1;
 var ri1 = "dice" + r1 +".png";
 var r1s = "images/" + ri1;
@@ -10,13 +13,15 @@ var r2s = "images/" + ri2;
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", r2s);
 
+var nc = document.querySelectorAll("p")[0];
+nc.innerHTML = p1;
 if(r1 > r2)
 {
-	document.querySelector("h1").innerHTML ="🚩 Player 1 wins!";
+	document.querySelector("h1").innerHTML ="🚩 "+p1+" wins!";
 }
 else if(r2 > r1)
 {
-	document.querySelector("h1").innerHTML ="Player 2 wins! 🚩";
+	document.querySelector("h1").innerHTML ="Robot wins! 🚩";
 }
 else
 {
